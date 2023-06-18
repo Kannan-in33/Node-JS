262443021b5177a169b31794dc53abb968d31657
Kannan-in33 <66407803+Kannan-in33@users.noreply.github.com>
2023-06-19 02:22:18 +0530

    Updating Existing files

--- server.js
+++ server.js
@@ -1,40 +1,102 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+
 
 const port = process.env.PORT || 5000 ;
 const server = express();
 
-server.get('/', (req, res) => res.send("Hey Node"));
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './pages/index.html'));
+
+});
+
+        // res.send("Hey Dude");
+        // return;
 
 server.use( (req, res)=>{
-    // console.log(req.path)
+    let obj ={};
+    let obj2 = [];
+    let company =[];
+    let obj3 = {};
+    let i = 0;
             if(req.path !== '/favicon.ico'){
-                    let obj;
-                        fs.readFile('.' + req.path + '.json', 'utf8', function (err, data) {
-                        if (err) throw err;
-                        obj = JSON.parse(data);
-                        let obj2 = {}
-                            for (const key in obj['datasets'][0]['values']) {
+                console.log(req.path);
+                        if(req.path == '/k'){        
+                                    const directorypath = path.join(__dirname, 'src');
+                                    fs.readdir(directorypath , function (err, files) {
+                                        if (err) throw err;
+                                        files.forEach( (file) => {
+                                            fs.readFile(path.join(directorypath , file), 'utf8', function (err2, data) {
+                                                if (err2) throw err2;
+                                                obj = JSON.parse(data);
+                                                i++;
+                                                // res.send(obj['datasets'][0]['values']);
+                                                for (let key in obj['datasets'][0]['values']) {
+                                                    obj2.push(obj['datasets'][0]['values'][key][1]);
 
-                                // console.log(obj['datasets'][0]['values'][key][1]);
+                                                }
+                                                if (i == files.length){  
+                                                    console.log(obj2.length / files.length); 
+                                                    res.send(obj2);
+                                                }
 
-                                obj2[key] = obj['datasets'][0]['values'][key][1]
+                                            });
 
-                                    if( ((obj['datasets'][0]['values']).length == Object.keys(obj2).length) || (Object.keys(obj2).length == 10)){
-                                        // res.send(obj['datasets'][0]['values'][key][1]);
-                                        res.send(obj2);
-                                        return;
-                                    }                    
+                                        });
 
+                                    });
+                                }
+
+                        if(req.path == '/company'){        
+                        const directorypath = path.join(__dirname, 'src');
+                        fs.readdir(directorypath , function (err, files) {
+                                    if (err) throw err;
+                                    files.forEach( (file) => {
+                                        obj2.push(file);
+                                        });
+                                        if(obj2.length > 0){
+                                            res.send(obj2);
+                                        }
+                                            
+                                        });
+
+                                    };
+
+
+                        if(req.path == '/g'){        
+                            const directorypath = path.join(__dirname, 'src');
+                            fs.readdir(directorypath , function (err, files) {
+                            if (err) throw err;
+                            files.forEach( (file) => {
+                            fs.readFile(path.join(directorypath , file), 'utf8', function (err2, data) {
+                            if (err2) throw err2;
+                                obj = JSON.parse(data);
+                                company.push(file);
+                            i++;
+                            for (let key in obj['datasets'][0]['values']) {
+                                obj2.push(obj['datasets'][0]['values'][key][1]);                            
                             }
+                            if (i == files.length ){
+                            obj3 = { "company" : company,
+                                    "values" : obj2
+                                    }
+                            res.send(obj3);
+                            }
+                            
+                            });
+                            
+                            });
+                            
+                            });
+                        }
                         
-                
-                        });
-        
-    
+                    
             }
+
 });
 
 server.listen(port, () => {
     console.log('Server is listening on port ' + port);
+    
 })
