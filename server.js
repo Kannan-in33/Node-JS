2c86752c3342e6a023064ced18f6c23035590bea
Kannan-in33 <kannan.in33@hotmail.com>
2023-06-23 03:23:01 +0530

    updated Volume details

--- server.js
+++ server.js
@@ -20,12 +20,83 @@ server.use( (req, res)=>{
     let company =[];
     let obj3 = {};
     let valueList = {};
+    let volumeList = {};
     companyObject = {};
     companyList =[];
     let i = 0;
     let max = 0;
             if(req.path !== '/favicon.ico'){
                 console.log(req.path);
+
+                if(req.path == '/10x'){
+
+                    // ************************************************
+
+                    const directorypath = path.join(__dirname, '101');
+                fs.readdir(directorypath , function (err, files) {
+                if (err) throw err;
+                files.forEach( (file) => {
+                    let k =0;
+                    fs.readFile(path.join(directorypath , file), 'utf8', function (err2, data) {
+                    if (err2) throw err2;
+                        obj = JSON.parse(data);
+                        company.push(file);
+                        valueList[file.split('.')[0]] = obj['datasets'][0]['values'].length
+                        max = max < obj['datasets'][1]['values'].length ? obj['datasets'][1]['values'].length : max
+                        i++;
+                        
+                       try{
+
+                        obj2[0] = obj['datasets'][1]['values'][max -1][1];
+                        obj2[1] = obj['datasets'][1]['values'][max -2][1];
+                       }
+                       catch{
+                        console.log(file.split('.')[0] );
+                        console.log(max);
+                       }
+
+                    // for (let key in obj['datasets'][1]['values']) {
+
+                        
+                    //     obj2.push(obj['datasets'][1]['values'][key][1]);
+
+                    //     k++;
+                    //     if(k == 2){
+                            if(file.split('.')[0] == 'ANANTRAJ'){
+                                console.log(obj2[0] + "   "+ obj2[1]);
+                                }
+                    //         break;
+                    //     }                         
+                    // }
+
+                    if((obj2[0]/obj2[1]) > 1){
+                    companyObject[file.split('.')[0]] = (obj2[0]/obj2[1]).toFixed(2);
+                    }
+                    // [...obj2];
+                    obj2 =[];
+
+                    if (i == files.length ){
+                    obj3 = { "company" : company,
+                            "values" : obj2,
+                            "valueList" : valueList,
+                            "max" : max,
+                            "companyObject" : companyObject
+                            }
+                    res.send(obj3);
+                }
+                
+                });
+                
+                });
+                
+                });
+
+                // *************************************************************
+
+                }
+
+                else{
+               
                 const directorypath = path.join(__dirname, req.path.replace('/', ''));
                 fs.readdir(directorypath , function (err, files) {
                 if (err) throw err;
@@ -51,8 +122,6 @@ server.use( (req, res)=>{
                             "max" : max,
                             "companyObject" : companyObject
                             }
-                    // console.log(companyObject['ASTRAMICRO']);
-                    
                     res.send(obj3);
                 }
                 
@@ -61,7 +130,7 @@ server.use( (req, res)=>{
                 });
                 
                 });
-                        
+            }         
                     
             }
 
