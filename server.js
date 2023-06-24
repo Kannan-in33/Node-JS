272205246845fdcd4aad2fc8991692353e03c4e1
Kannan-in33 <kannan.in33@hotmail.com>
2023-06-24 06:06:28 +0530

    30 days Average with anchor tag

--- server.js
+++ server.js
@@ -21,74 +21,55 @@ server.use( (req, res)=>{
     let obj3 = {};
     let valueList = {};
     let volumeList = {};
-    companyObject = {};
-    companyList =[];
-    let i = 0;
+    let companyObject = {};
+    let companyList =[];
+    
     let max = 0;
-            if(req.path !== '/favicon.ico'){
-                console.log(req.path);
+    let  companyObject2 ={};
 
-                if(req.path == '/10x'){
+if(req.path !== '/favicon.ico'){
+if(req.path == '/10x'){
+let j = 0;
+let foldersPath = fs.readdirSync(path.resolve(__dirname, 'src'));
 
-                    // ************************************************
-
-                    const directorypath = path.join(__dirname, '101');
-                fs.readdir(directorypath , function (err, files) {
-                if (err) throw err;
-                files.forEach( (file) => {
-                    let k =0;
+        foldersPath.forEach( (folder, j) => {
+            const directorypath = path.join(__dirname, 'src/' + folder);
+            fs.readdir(directorypath , function (err, files) {
+             if (err) throw err;
+            files.forEach( (file, i) => {
                     fs.readFile(path.join(directorypath , file), 'utf8', function (err2, data) {
                     if (err2) throw err2;
-                        obj = JSON.parse(data);
-                        company.push(file);
-                        valueList[file.split('.')[0]] = obj['datasets'][0]['values'].length
-                        max = max < obj['datasets'][1]['values'].length ? obj['datasets'][1]['values'].length : max
-                        i++;
-                        
-                       try{
-
+                    obj = JSON.parse(data);
+                    company.push(file);
+                    valueList[file.split('.')[0]] = obj['datasets'][0]['values'].length
+                    max = max < obj['datasets'][1]['values'].length ? obj['datasets'][1]['values'].length : max
+                    try{
                         obj2[0] = obj['datasets'][1]['values'][max -1][1];
                         obj2[1] = obj['datasets'][1]['values'][max -2][1];
-                       }
-                       catch{
-                        console.log(file.split('.')[0] );
-                        console.log(max);
-                       }
+                    }catch{ }
+                            if((obj2[0]/obj2[1]) > 3){
+                            companyObject[file.split('.')[0]] = (obj2[0]/obj2[1]).toFixed(2);
+                            }
 
-
-
-                    if((obj2[0]/obj2[1]) > 3){
-                    companyObject[file.split('.')[0]] = (obj2[0]/obj2[1]).toFixed(2);
-                    }
-                    // [...obj2];
-                    obj2 =[];
-
-                    if (i == files.length ){
-                    obj3 = { "company" : company,
-                            "values" : obj2,
-                            "valueList" : valueList,
-                            "max" : max,
+                            if( j == foldersPath.length -1 && i == files.length -1){
+                            obj3 = { 
                             "companyObject" : companyObject
                             }
-                    res.send(obj3);
-                }
-                
-                });
-                
-                });
-                
-                });
+                            console.log(obj3);
+                            res.send(obj3);
+                            }
 
-                // *************************************************************
+        });   });  });  })
 
-                }
+
+}
 
                 else{
                
-                const directorypath = path.join(__dirname, req.path.replace('/', ''));
+                const directorypath = path.join(__dirname, req.path.replace('/', 'src/' + ''));
                 fs.readdir(directorypath , function (err, files) {
                 if (err) throw err;
-                files.forEach( (file) => {
+                files.forEach( (file, i) => {
                     fs.readFile(path.join(directorypath , file), 'utf8', function (err2, data) {
                     if (err2) throw err2;
                         obj = JSON.parse(data);
@@ -96,14 +77,13 @@ server.use( (req, res)=>{
 
                         valueList[file.split('.')[0]] = obj['datasets'][0]['values'].length
                         max = max < obj['datasets'][0]['values'].length ? obj['datasets'][0]['values'].length : max
-                        i++;
                     for (let key in obj['datasets'][0]['values']) {
                         obj2.push(obj['datasets'][0]['values'][key][1]);                           
                     }
                     companyObject[file.split('.')[0]] = [...obj2];
                     obj2 =[];
 
-                    if (i == files.length ){
+                    if (i == files.length -1 ){
                     obj3 = { "company" : company,
                             "values" : obj2,
                             "valueList" : valueList,
