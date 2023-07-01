1cae0e4a09ee0047941732fdc2f970ade3e181bc
Kannan-in33 <kannan.in33@hotmail.com>
2023-07-02 03:11:20 +0530

    Updated GA 4 code

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ server.get('/', (req, res) => {
 
 });
 
-        // res.send("Hey Dude");
-        // return;
-
+        
 server.use( (req, res)=>{
     let obj ={};
     let obj2 = [];
@@ -24,45 +22,50 @@ server.use( (req, res)=>{
     let companyObject = {};
     let companyList =[];
     
+    
     let max = 0;
     let  companyObject2 ={};
 
+
+
 if(req.path !== '/favicon.ico'){
-if(req.path == '/10x'){
-let j = 0;
-let foldersPath = fs.readdirSync(path.resolve(__dirname, 'src'));
 
-        foldersPath.forEach( (folder, j) => {
-            const directorypath = path.join(__dirname, 'src/' + folder);
-            fs.readdir(directorypath , function (err, files) {
-             if (err) throw err;
-            files.forEach( (file, i) => {
-                    fs.readFile(path.join(directorypath , file), 'utf8', function (err2, data) {
-                    if (err2) throw err2;
-                    obj = JSON.parse(data);
-                    company.push(file);
-                    valueList[file.split('.')[0]] = obj['datasets'][0]['values'].length
-                    max = max < obj['datasets'][1]['values'].length ? obj['datasets'][1]['values'].length : max
-                    try{
-                        obj2[0] = obj['datasets'][1]['values'][max -1][1];
-                        obj2[1] = obj['datasets'][1]['values'][max -2][1];
-                    }catch{ }
-                            if((obj2[0]/obj2[1]) > 3){
-                            companyObject[file.split('.')[0]] = (obj2[0]/obj2[1]).toFixed(2);
-                            }
+ if(req.path == '/10x'){
+        let j = 0;
+        let foldersPath = fs.readdirSync(path.resolve(__dirname, 'src'));
 
-                            if( j == foldersPath.length -1 && i == files.length -1){
-                            obj3 = { 
-                            "companyObject" : companyObject
-                            }
-                            console.log(obj3);
-                            res.send(obj3);
-                            }
+                foldersPath.forEach( (folder, j) => {
+                    const directorypath = path.join(__dirname, 'src/' + folder);
+                    fs.readdir(directorypath , function (err, files) {
+                    if (err) throw err;
+                    files.forEach( (file, i) => {
+                            fs.readFile(path.join(directorypath , file), 'utf8', function (err2, data) {
+                            if (err2) throw err2;
+                            obj = JSON.parse(data);
+                            company.push(file);
+                            valueList[file.split('.')[0]] = obj['datasets'][0]['values'].length
+                            max = max < obj['datasets'][1]['values'].length ? obj['datasets'][1]['values'].length : max
+                            try{
+                                obj2[0] = obj['datasets'][1]['values'][max -1][1];
+                                obj2[1] = obj['datasets'][1]['values'][max -2][1];
+                            }catch{ }
+                                    if((obj2[0]/obj2[1]) > 3){
+                                    companyObject[file.split('.')[0]] = (obj2[0]/obj2[1]).toFixed(2);
+                                    }
+
+                                    if( j == foldersPath.length -1 && i == files.length -1){
+                                    obj3 = { 
+                                    "companyObject" : companyObject
+                                    }
+                                    console.log(obj3);
+                                    res.send(obj3);
+                                    }
 
         });   });  });  })
 
 
 }
+// 10x Condition Ends here
 
                 else{
                
