47069c6dec87c518f563f6c1d222804e76fb39ac
Kannan-in33 <66407803+Kannan-in33@users.noreply.github.com>
2023-08-01 10:01:36 +0530

    Update scripts.js
    Reverted to old 30 code

--- pages/scripts.js
+++ pages/scripts.js
@@ -26,10 +26,6 @@ let positiveCompany3 = {};
 let negativeList3 = [];
 let negativeCompany3 = {};
 let CurrentPriceObj = {};
-let c ;
-let h ;
-let w ;
-let ctx ;
 
 const weekday = ["S","M","T","W","Th","F","St"];
 
@@ -510,7 +506,6 @@ let resultCount = 0;
                             // console.log(key + "    " + ( (Number(buyAvg[20]) + Number(buyAvg[29]) + Number(buyAvg[15]) )/3).toFixed(2) + "    " + ccurrect);
                           }
 
-
                       
                         new Chart(canvas, {
                             type: "line",
@@ -525,8 +520,6 @@ let resultCount = 0;
                                     }]
                                   },  
                                   options: {
-                                    animation: false,
-                                    normalized: true,
                                       scales: {
                                           yAxes: [{
                                               ticks: {
@@ -555,8 +548,6 @@ let resultCount = 0;
                                       }]
                                     },
                               options: {
-                                    animation: false,
-                                    normalized: true,
                                       plugins: {
                                           legend: true // Hide legend
                                       },
@@ -918,7 +909,7 @@ let myFunc2 = letsDebounce(setFilter2,1000);
       ele.style.display = "none" ;
     });
    //positive0.length - 1;
-      for(let i = 0; i < Math.min((30, positive0.length - 1);){
+      for(let i = 0; i < 30;){
       let indx = positiveCompany0[positive0[i]];
       // console.log(i + "   " + indx);
       let elementUp = document.getElementById(indx);
@@ -942,7 +933,7 @@ function getDown0(){
   });
 
 // negativeList0.length  - 1
-  for(let i = 0; i < Math.min((50, negativeList0.length  - 1); ){
+  for(let i = 0; i < 80; ){
     let indx = negativeCompany0[negativeList0[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -966,7 +957,7 @@ function getUp1(){
     ele.style.display = "none" ;
   });
 //  positive1.length  - 1
-    for(let i = 0; i < Math.min((30, positive1.length - 1);){
+    for(let i = 0; i < 30;){
     let indx = positiveCompany1[positive1[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -990,7 +981,7 @@ lst.forEach( (ele) => {
 });
 
 // negativeList1.length  - 1
-for(let i = 0; i < Math.min((50, negativeList1.length  - 1); ){
+for(let i = 0; i < 80; ){
   let indx = negativeCompany1[negativeList1[i]];
   // console.log(i + "   " + indx);
   let elementUp = document.getElementById(indx);
@@ -1012,7 +1003,7 @@ function getUp2(){
     ele.style.display = "none" ;
   });
 //  positive2.length  - 1
-    for(let i = 0; i < Math.min((30, positive2.length - 1);){
+    for(let i = 0; i < 30;){
     let indx = positiveCompany2[positive2[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -1036,7 +1027,7 @@ lst.forEach( (ele) => {
 });
 
 // negativeList2.length  - 1
-for(let i = 0; i < Math.min((50, negativeList2.length  - 1); ){
+for(let i = 0; i < 80; ){
   let indx = negativeCompany2[negativeList2[i]];
   // console.log(i + "   " + indx);
   let elementUp = document.getElementById(indx);
@@ -1058,7 +1049,7 @@ function getUp3(){
     ele.style.display = "none" ;
   });
 //  positive3.length  - 1
-    for(let i = 0; i < Math.min((30, positive3.length - 1);){
+    for(let i = 0; i < 80;){
     let indx = positiveCompany3[positive3[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -1082,7 +1073,7 @@ lst.forEach( (ele) => {
 });
 
 
-for(let i = 0; i < Math.min((50, negativeList3.length  - 1); ){
+for(let i = 0; i < negativeList3.length  - 1; ){
   let indx = negativeCompany3[negativeList3[i]];
   // console.log(i + "   " + indx);
   let elementUp = document.getElementById(indx);
@@ -1184,88 +1175,4 @@ function clearFavourits(){
     };
   }
   
-  // -------------------------------------------------------------------------------------
-  // ------------------------------------------------------------------------------------------------------
-  // ----------------------------------------------------------------------------------------
-
-  // var chart = document.getElementById("chart").getContext("2d");
-
-  // chart.canvas.width = "300";
-  // chart.canvas.height = "150";
   
-  // var cw = chart.canvas.width;
-  // var ch = chart.canvas.height;
-  
-  // var w = [0,cw/5,2*(cw/5),3*(cw/5),4*(cw/5),5*(cw/5)];
-  
-  // var h = [ch,ch-ch/5,ch-2*(ch/5),ch-3*(ch/5),ch-4*(ch/5),ch-5*(ch/5)];
-  
-  // var weeks = ['week1','week2','week3','week4'];
-  // var values = [100,200,(50,400,500];
-  // var val = [];
-  
-  
-  // for(var i = 0;i<weeks.length;i++){
-  //   var week = document.createElement('span');
-  //   var text = document.createTextNode(weeks[i])
-  //   week.appendChild(text);
-  //   document.getElementById('weeks').appendChild(week);
-  // }
-  
-  // for(var i = values.length-1;i>=0;i--){
-  //   var value = document.createElement('span');
-  //   var text_value = document.createTextNode(values[i])
-  //   value.appendChild(text_value);
-  //   document.getElementById('values').appendChild(value);
-  // }
-  
-  // var ch = document.getElementById("chart");
-  
-  // chart.beginPath();
-        
-  // //actual graph      
-  // for(var i =0;i<w.length;i++){
-  
-  //   chart.moveTo(0, ch);
-  //   chart.strokeStyle = '#1dd2af';
-  //   chart.lineWidth = 2;
-  //   chart.lineTo(w[i], h[Math.floor((Math.random() * 4) + 1)]);
-  //   chart.stroke();
-  // }
-  
-  // gridV();
-  // gridH();
-  
-
-  // // vertical lines
-  // function gridV(){
-    
-  //   for(var i =1;i<w.length-1;i++){
-  //     chart.strokeStyle = 'rgba(29, 210, 175,0.3)';
-  //     chart.lineWidth = 1;
-  //     chart.moveTo(w[i], 0);
-  //     chart.lineTo(w[i], 150);
-  //     }
-  //         chart.stroke();
-  //       }
-  
-  // //horizontal lines
-  // function gridH(){
-    
-  //   for(var i =1;i<h.length-1;i++){
-  //     chart.strokeStyle = 'rgba(29, 210, 175,0.3)';
-  //     chart.lineWidth = 1;
-  //     chart.moveTo(0,h[i]);
-  //     chart.lineTo(3000,h[i]);
-  //     }
-  //         chart.stroke();
-  //       }
-  
-    
-
-  
-  // var beforex = document.querySelector('#weeks>span:before');
-  
-  // for(var i = 1;i<w.length;i++){
-  //   beforex.style.marginLeft = w[i];
-  // }
