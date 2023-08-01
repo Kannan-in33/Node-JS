a1613e124f454ea99349e8bdce05fe56d943e086
Kannan-in33 <kannan.in33@hotmail.com>
2023-08-01 08:04:41 +0530

    Data for 31 July

--- pages/scripts.js
+++ pages/scripts.js
@@ -26,6 +26,10 @@ let positiveCompany3 = {};
 let negativeList3 = [];
 let negativeCompany3 = {};
 let CurrentPriceObj = {};
+let c ;
+let h ;
+let w ;
+let ctx ;
 
 const weekday = ["S","M","T","W","Th","F","St"];
 
@@ -506,6 +510,24 @@ let resultCount = 0;
                             // console.log(key + "    " + ( (Number(buyAvg[20]) + Number(buyAvg[29]) + Number(buyAvg[15]) )/3).toFixed(2) + "    " + ccurrect);
                           }
 
+
+let c = canvas;
+let h = c.height
+let w = c.width
+let ctx = c.getContext('2d')
+// dataY = [...yValues2];
+// dataX =  [...xValues];
+un = Math.round(Math.max(...xValues)/15)
+un = Math.round((Math.max(...xValues)-Math.min(...xValues))/10)
+ys = (w-40)/yValues2.length
+dataT = []
+
+chartLine()
+digram()
+data()
+draw()
+pointes()
+
                       
                         new Chart(canvas, {
                             type: "line",
@@ -913,7 +935,7 @@ let myFunc2 = letsDebounce(setFilter2,1000);
       ele.style.display = "none" ;
     });
    //positive0.length - 1;
-      for(let i = 0; i < Math.min(300, positive0.length - 1);){
+      for(let i = 0; i < Math.min((30, positive0.length - 1);){
       let indx = positiveCompany0[positive0[i]];
       // console.log(i + "   " + indx);
       let elementUp = document.getElementById(indx);
@@ -937,7 +959,7 @@ function getDown0(){
   });
 
 // negativeList0.length  - 1
-  for(let i = 0; i < Math.min(300, negativeList0.length  - 1); ){
+  for(let i = 0; i < Math.min((50, negativeList0.length  - 1); ){
     let indx = negativeCompany0[negativeList0[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -961,7 +983,7 @@ function getUp1(){
     ele.style.display = "none" ;
   });
 //  positive1.length  - 1
-    for(let i = 0; i < Math.min(250, positive1.length - 1);){
+    for(let i = 0; i < Math.min((30, positive1.length - 1);){
     let indx = positiveCompany1[positive1[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -985,7 +1007,7 @@ lst.forEach( (ele) => {
 });
 
 // negativeList1.length  - 1
-for(let i = 0; i < Math.min(300, negativeList1.length  - 1); ){
+for(let i = 0; i < Math.min((50, negativeList1.length  - 1); ){
   let indx = negativeCompany1[negativeList1[i]];
   // console.log(i + "   " + indx);
   let elementUp = document.getElementById(indx);
@@ -1007,7 +1029,7 @@ function getUp2(){
     ele.style.display = "none" ;
   });
 //  positive2.length  - 1
-    for(let i = 0; i < Math.min(250, positive2.length - 1);){
+    for(let i = 0; i < Math.min((30, positive2.length - 1);){
     let indx = positiveCompany2[positive2[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -1031,7 +1053,7 @@ lst.forEach( (ele) => {
 });
 
 // negativeList2.length  - 1
-for(let i = 0; i < Math.min(300, negativeList2.length  - 1); ){
+for(let i = 0; i < Math.min((50, negativeList2.length  - 1); ){
   let indx = negativeCompany2[negativeList2[i]];
   // console.log(i + "   " + indx);
   let elementUp = document.getElementById(indx);
@@ -1053,7 +1075,7 @@ function getUp3(){
     ele.style.display = "none" ;
   });
 //  positive3.length  - 1
-    for(let i = 0; i < Math.min(250, positive3.length - 1);){
+    for(let i = 0; i < Math.min((30, positive3.length - 1);){
     let indx = positiveCompany3[positive3[i]];
     // console.log(i + "   " + indx);
     let elementUp = document.getElementById(indx);
@@ -1077,7 +1099,7 @@ lst.forEach( (ele) => {
 });
 
 
-for(let i = 0; i < Math.min(300, negativeList3.length  - 1); ){
+for(let i = 0; i < Math.min((50, negativeList3.length  - 1); ){
   let indx = negativeCompany3[negativeList3[i]];
   // console.log(i + "   " + indx);
   let elementUp = document.getElementById(indx);
@@ -1179,4 +1201,88 @@ function clearFavourits(){
     };
   }
   
+  // -------------------------------------------------------------------------------------
+  // ------------------------------------------------------------------------------------------------------
+  // ----------------------------------------------------------------------------------------
+
+  // var chart = document.getElementById("chart").getContext("2d");
+
+  // chart.canvas.width = "300";
+  // chart.canvas.height = "150";
   
+  // var cw = chart.canvas.width;
+  // var ch = chart.canvas.height;
+  
+  // var w = [0,cw/5,2*(cw/5),3*(cw/5),4*(cw/5),5*(cw/5)];
+  
+  // var h = [ch,ch-ch/5,ch-2*(ch/5),ch-3*(ch/5),ch-4*(ch/5),ch-5*(ch/5)];
+  
+  // var weeks = ['week1','week2','week3','week4'];
+  // var values = [100,200,(50,400,500];
+  // var val = [];
+  
+  
+  // for(var i = 0;i<weeks.length;i++){
+  //   var week = document.createElement('span');
+  //   var text = document.createTextNode(weeks[i])
+  //   week.appendChild(text);
+  //   document.getElementById('weeks').appendChild(week);
+  // }
+  
+  // for(var i = values.length-1;i>=0;i--){
+  //   var value = document.createElement('span');
+  //   var text_value = document.createTextNode(values[i])
+  //   value.appendChild(text_value);
+  //   document.getElementById('values').appendChild(value);
+  // }
+  
+  // var ch = document.getElementById("chart");
+  
+  // chart.beginPath();
+        
+  // //actual graph      
+  // for(var i =0;i<w.length;i++){
+  
+  //   chart.moveTo(0, ch);
+  //   chart.strokeStyle = '#1dd2af';
+  //   chart.lineWidth = 2;
+  //   chart.lineTo(w[i], h[Math.floor((Math.random() * 4) + 1)]);
+  //   chart.stroke();
+  // }
+  
+  // gridV();
+  // gridH();
+  
+
+  // // vertical lines
+  // function gridV(){
+    
+  //   for(var i =1;i<w.length-1;i++){
+  //     chart.strokeStyle = 'rgba(29, 210, 175,0.3)';
+  //     chart.lineWidth = 1;
+  //     chart.moveTo(w[i], 0);
+  //     chart.lineTo(w[i], 150);
+  //     }
+  //         chart.stroke();
+  //       }
+  
+  // //horizontal lines
+  // function gridH(){
+    
+  //   for(var i =1;i<h.length-1;i++){
+  //     chart.strokeStyle = 'rgba(29, 210, 175,0.3)';
+  //     chart.lineWidth = 1;
+  //     chart.moveTo(0,h[i]);
+  //     chart.lineTo(3000,h[i]);
+  //     }
+  //         chart.stroke();
+  //       }
+  
+    
+
+  
+  // var beforex = document.querySelector('#weeks>span:before');
+  
+  // for(var i = 1;i<w.length;i++){
+  //   beforex.style.marginLeft = w[i];
+  // }
