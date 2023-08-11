64387f9562664f6bfc066a4c8de3efb14ec558f9
Kannan-in33 <66407803+Kannan-in33@users.noreply.github.com>
2023-08-11 15:24:56 +0530

    Update scripts for loop to reverse

--- pages/scripts.js
+++ pages/scripts.js
@@ -713,7 +713,7 @@ let resultCount = 0;
                 }
             });
             
-            for(let i = 0; i < currentPriceDataTable[key].length; i++){
+            for(let i = currentPriceDataTable[key].length; i> 0; i--){
 
             let barc = document.createElement("div");
               barc.setAttribute("class", "barc");
